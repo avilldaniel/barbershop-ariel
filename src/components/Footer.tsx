2a4336df89e45e3d2b6c930c1da5b05d822e02ee
Daniel Villegas <avill.daniel@gmail.com>
2022-08-18 10:31:21 -0700

    ci: drafting footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,8 +5,17 @@ const Footer = () => {
     <footer>
       <main>
         footer:
-        <div>Contact</div>
-        <div>Hours</div>
+        <div>
+          Contact <br />
+          Ph: (702)XXX-XXXX <br />
+          Email: email@gmail.com
+        </div>
+        <div>
+          Hours <br />
+          Mon-Thurs: xAM - yPM <br />
+          Fri-Sat: xAM - yPM <br />
+          Sun: Closed
+        </div>
         <div>Logo</div>
       </main>
 
